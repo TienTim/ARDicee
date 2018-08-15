0ed57fe30a2075babb31024e6bbb246ad12ee227
Tien Do <tien.corbett@gmail.com>
2018-08-15 23:49:17 +0700

    Moon

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 //        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
         let sphere = SCNSphere(radius: 0.2)
         let material = SCNMaterial()
-        material.diffuse.contents = UIImage(named: "art.scnassets/8k_earth_daymap.jpg")
+        material.diffuse.contents = UIImage(named: "art.scnassets/8k_moom.jpg")
         sphere.materials = [material]
         let node = SCNNode()
         node.position = SCNVector3(0, 0.1, -0.5)
