01e2fee27ecb929c8ec169957533a7135579fc9c
Tien Do <tien.corbett@gmail.com>
2018-08-15 23:33:54 +0700

    Debug points

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -21,6 +21,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         sceneView.delegate = self
         sceneView.showsStatistics = true
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        sceneView.autoenablesDefaultLighting = true
         
     }
     
